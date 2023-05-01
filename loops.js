02c7374cb19603fedaa93fe97e8de2eb42300162
Haleh Bozorgnia <bozorgnia2010@gmail.com>
2023-04-30 21:42:38 -0700

    finished loop practice

---
+++ loops.js
@@ -0,0 +1,46 @@
+for (let i = 0; i < 100; i++) {
+  console.log(i);
+}
+
+for (let i = 99; i >= 0; i--) {
+  console.log(i);
+}
+
+for (let i = 0; i < 100; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
+
+for (let i = 99; i >= 0; i--) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
+
+for (let i = 0; i < 100; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+for (let i = 99; i >= 0; i--) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+for (let i = 49; i <= 72; i++) {
+  console.log(i);
+}
+
+for (let i = 81; i >= 26; i--) {
+  console.log(i);
+}
+
+for (let i = 1; i <= 90; i++) {
+  let multi = i * 3;
+  if (multi <= 90) {
+    console.log(multi);
+  }
+}
